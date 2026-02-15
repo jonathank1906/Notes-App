717942109645a30cb63569c60a6f6eb5e4db9cc7
jonathank1906 <jonathankleffel@verizon.net>
2026-02-15 12:58:18 +0100

    Open Slide Notes from index.html

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, session } = require('electron')
+const { app, BrowserWindow, session, ipcMain } = require('electron')
 const path = require('path')
 
 function createWindow () {
@@ -6,25 +6,35 @@ function createWindow () {
     width: 1200,
     height: 800,
     webPreferences: {
-      nodeIntegration: true, // Allows you to use 'require' in your HTML
-      contextIsolation: false // Disables the sandbox so you can save files
+      nodeIntegration: true,
+      contextIsolation: false
     }
-  })
-
-  // Completely destroy the default menu so the Alt key doesn't trigger it
-  //win.setMenu(null)
-
-  // Load your HTML file (Change 'index.html' to 'MindMap.html' if you kept the old name!)
-  win.loadFile(path.join(__dirname, 'index.html'))
-  
-  // Optional: Uncomment the line below if you ever need Developer Tools back
-  // win.webContents.openDevTools()
+  });
+  win.loadFile(path.join(__dirname, 'index.html'));
 }
 
+function openSlideNotesWindow() {
+  console.log('Creating Slide Notes window...');
+  const slideWin = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  slideWin.loadFile(path.join(__dirname, 'Slide Notes.html'));
+}
+
+// IPC handler for opening Slide Notes window
+ipcMain.on('open-slide-notes-window', () => {
+  console.log('IPC: open-slide-notes-window received');
+  openSlideNotesWindow();
+});
+
+
 app.whenReady().then(() => {
-  
   // --- YOUTUBE ERROR 153 FIX ---
-  // Intercept network requests and inject a fake Referer header so YouTube doesn't block the embed
   session.defaultSession.webRequest.onBeforeSendHeaders(
     { urls: ['*://*.youtube.com/*', '*://*.youtube-nocookie.com/*'] },
     (details, callback) => {
@@ -32,16 +42,14 @@ app.whenReady().then(() => {
       callback({ requestHeaders: details.requestHeaders });
     }
   );
-  // -----------------------------
-
-  createWindow()
-
+  createWindow();
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+  // (No longer needed: global.openSlideNotesWindow)
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
